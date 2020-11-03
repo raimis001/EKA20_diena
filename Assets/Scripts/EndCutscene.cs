e5b569ab22089f337157797a7b39ddcf9ba0c432
Raimonds <raimonds.udris@exonicus.com>
2020-11-03 13:45:26 +0200

    3.10

---
+++ Assets/Scripts/EndCutscene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCutscene : MonoBehaviour
+{
+    
+    public void LoadGame() 
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+}
